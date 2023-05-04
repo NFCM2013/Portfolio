64e562f626cc007e1ccace62e4ca7e1a37813c86
NFCM2013 <130025296+NFCM2013@users.noreply.github.com>
2023-05-04 17:03:16 +0900

    Add files via upload

---
+++ webスクレイピング.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+# スクレイピング対象のURLを指定
+url = 'スクレイピング対象のURL'
+
+# URLからHTMLを取得する
+response = requests.get(url)
+html = response.text
+
+# BeautifulSoupを使用してHTMLを解析する
+soup = BeautifulSoup(html, 'html.parser')
+
+# 特定の要素を抽出する例
+title = soup.title.text  # ページのタイトルを取得する
+links = soup.find_all('a')  # すべてのリンクを取得する
+
+# 抽出したデータを出力する
+print(title)
+for link in links:
+    print(link.get('href'))
