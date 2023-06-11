f14551d493cd5d112b1d55ca252204980d2f981c
NFCM2013 <130025296+NFCM2013@users.noreply.github.com>
2023-06-11 19:05:25 +0900

    Add files via upload

---
+++ VIX_PreFC.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+# VIX指数の取得
+vix_data = yf.download('^VIX', start='2000-01-01', end=datetime.today().strftime('%Y-%m-%d'))
+
+# 終値データの取得
+close_prices = vix_data['Close']
+
+# グラフの作成
+plt.figure(figsize=(10, 6))
+plt.plot(close_prices)
+plt.title('VIX Index (Pre-Financial Crisis)')
+plt.xlabel('Date')
+plt.ylabel('VIX Index')
+plt.grid(True)
+plt.show()
